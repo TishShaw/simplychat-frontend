14c02092294665ced521f54f75c16c484e7b1530
Tishhshaw <Tish.shaw@yahoo.com>
2022-02-17 02:25:17 -0500

    added changes to Features component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
-import NavBar from './components/NavBar';
+import NavBar from './components/NavBar/NavBar';
 import Homepage from './pages/Homepage';
 
 function App() {
--- src/components/Features/Features.js
+++ src/components/Features/Features.js
@@ -7,13 +7,44 @@ function Features(props) {
 	const [product, setProduct] = useState([]);
 
 	useEffect(() => {
-		axios.get('http://localhost:8000/shop/').then((res) => console.log(res.data));
-	}, []);
+		axios.get('http://localhost:8000/shop/').then((res) =>{
+            console.log(res.data)
+            setProduct(res.data)
+        } 
+    )}, []);
 
 	return (
 		<div className='features'>
-			<h1 className='features-title'>Best Sellers</h1>
-			<div></div>
+			<h1 className='features-title'>
+				Best<span className='span'>Sellers</span>
+			</h1>
+			<div className='best'>
+				<div className='best-item'>
+					<img className='best-image' src={product[1].image} alt='' />
+					<p className='best-name'>{product[1].item}</p>
+					<p className='best-price'>{product[1].price}</p>
+				</div>
+				<div className='best-item'>
+					<img className='best-image' src={product[3].image} alt='' />
+					<p className='best-name'>{product[3].item}</p>
+					<p className='best-price'>{product[3].price}</p>
+				</div>
+				<div className='best-item'>
+					<img className='best-image' src={product[13].image} alt='' />
+					<p className='best-name'>{product[13].item}</p>
+					<p className='best-price'>{product[13].price}</p>
+				</div>
+				<div className='best-item'>
+					<img className='best-image' src={product[8].image} alt='' />
+					<p className='best-name'>{product[8].item}</p>
+					<p className='best-price'>{product[8].price}</p>
+				</div>
+				<div className='best-item'>
+					<img className='best-image' src={product[12].image} alt='' />
+					<p className='best-name'>{product[12].item}</p>
+					<p className='best-price'>{product[12].price}</p>
+				</div>
+			</div>
 			<Button dark outline>
 				Shop Best Sellers
 			</Button>
