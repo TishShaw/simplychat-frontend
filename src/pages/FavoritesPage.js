c3ababddc5225e22168ac86c4fd36daf16038595
Tishtanya Shaw <tish@tishtanyas-air.mynetworksettings.com>
2023-11-24 23:43:41 -0500

    Add favorites page

--- src/pages/FavoritesPage.js
+++ src/pages/FavoritesPage.js
@@ -1,14 +1,7 @@
 import React from 'react';
-import Favorites from '../Favorites/Favorites';
-import FavoritesHero from '../Hero/FavoritesHero';
 
 function FavoritesPage(props) {
-    return (
-        <div>
-            <FavoritesHero />
-            <Favorites />
-        </div>
-    );
+	return <div>FavoritesPage</div>;
 }
 
 export default FavoritesPage;
